a5462a6281ee2b67f49223fec232c82dcefc60b3
asunm1709212206102 <gunasekarguna109@gmail.com>
2026-05-27 15:52:51 +0530

    floop.java

--- floop.java
+++ floop.java
@@ -1,6 +1,9 @@
 public class floop {
     public static void main(String[] args) {
         for (int i = 0; i <= 10; i++) {
+            if(i==5) {
+                continue; // Skip the rest of the loop when i is 5
+            }
             System.out.println(i + "  ");
         }
     }
